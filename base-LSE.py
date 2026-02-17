96fcc25eef7dc5f908a45ec159215d315e6a4f68
JulioSierra17 <145613680+JulioSierra17@users.noreply.github.com>
2026-02-17 18:31:01 -0500

    Add files via upload

--- base-LSE.py
+++ base-LSE.py
@@ -5,28 +5,64 @@
 
 # Clase Nodo
 class Nodo:
-	def __init__(self, data):
-		self.data = data
-		self.siguiente = None
+    def __init__(self, data):
+        self.data = data
+        self.siguiente = None
 
-# CLase Listas enlazada simple
+#clase lista
 class ListaSE:
-	def __init__(self):
-		self.cabeza = None
+    def __init__(self):
+        self.cabeza = None
   
-  	# Lista Vacia
-	def vacio(self):
-		if self.cabeza == None:
-			print("Está vacia")
-		else:
-			print("Lista no vacia")
+    def vacio(self):
+        if self.cabeza is None:
+            print("Está vacia")
+        else:
+            print("Lista no vacia")
 
-	# Agregar al inicio
-	def agregarInicio(self, data):
-		nuevo_nodo = Nodo(data)
-		if self.cabeza is None:
-			self.cabeza = nuevo_nodo
-			return
-		else:
-			nuevo_nodo.siguiente = self.cabeza
-			self.cabeza = nuevo_nodo
+    def agregarInicio(self, data):
+        nuevo_nodo = Nodo(data)
+        nuevo_nodo.siguiente = self.cabeza
+        self.cabeza = nuevo_nodo
+
+    # contar el numero de elementos
+    def contar(self):
+        contador = 0
+        actual = self.cabeza
+
+        while actual:
+            contador += 1
+            actual = actual.siguiente
+
+        return contador
+
+    # buscar un elemento
+    def buscar(self, data):
+        actual = self.cabeza
+
+        while actual:
+            if actual.data == data:
+                return True
+            actual = actual.siguiente
+
+        return False
+
+    # eliminar el ultimo elemento
+    def eliminarUltimo(self):
+
+        if self.cabeza is None:
+            return
+
+        if self.cabeza.siguiente is None:
+            self.cabeza = None
+            return
+
+        actual = self.cabeza
+
+        while actual.siguiente.siguiente:
+            actual = actual.siguiente
+        actual.siguiente = None
+	# eliminar el primer elemento    def eliminarPrimero(self):
+        if self.cabeza is None:
+            return
+        self.cabeza = self.cabeza.siguiente
